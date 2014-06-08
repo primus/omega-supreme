2dad656919a8a277f5bdb1565c4bce8b4de24583
3rd-Eden <info@3rd-Eden.com>
2014-06-08 14:20:31 +0200

    [test] spin up 2 servers

--- test/supreme.test.js
+++ test/supreme.test.js
@@ -6,22 +6,35 @@ describe('omega supreme', function () {
     , omega = require('../');
 
   var port = 1024
+    , server2
     , server
+    , http2
     , http;
 
   beforeEach(function each(next) {
     http = require('http').createServer();
+    http2 = require('http').createServer();
 
     server = new Primus(http, {
       transformer: 'websockets'
     });
 
+    server2 = new Primus(http2, {
+      transformer: 'websockets'
+    });
+
     http.port = port++;
-    http.listen(http.port, next);
+    http2.port = port++;
+
+    http.listen(http.port, function () {
+      http2.listen(http2.port, next);
+    });
   });
 
   afterEach(function each(next) {
-    server.destroy(next);
+    server.destroy(function () {
+      server2.destroy(next);
+    });
   });
 
   it('is exposed as function', function () {
@@ -31,6 +44,26 @@ describe('omega supreme', function () {
   it('exposes a plugin interface', function () {
     assume(omega.server).to.be.a('function');
     server.use('omega-supreme', omega);
+  });
+
+  it('adds a middleware layer', function () {
+    assume(!~server.indexOfLayer('omega-supreme')).to.be.true();
+    server.use('omega-supreme', omega);
+    assume(!!~server.indexOfLayer('omega-supreme')).to.be.true();
+  });
+
+  describe('.options', function () {
+    it('returns a object with our defaults', function () {
+      assume(omega.options()).to.be.a('object');
+      assume(omega.options().method).to.be.a('string');
+    });
+
+    it('uppercases methods', function () {
+      var options = omega.options({ method: 'get' });
+
+      assume(options.method).to.equal('GET');
+      assume(omega.options().method).to.equal('PUT');
+    });
   });
 
   describe('.forward', function () {
