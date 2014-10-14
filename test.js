5682758048110dc245ea163cfbf59028eeca96b7
Luigi Pinca <luigipinca@gmail.com>
2014-10-14 22:49:25 +0200

    [test] Add more tests

--- test.js
+++ test.js
@@ -2,43 +2,46 @@
 describe('omega supreme', function () {
   'use strict';
 
-  var assume = require('assume')
+  var url = require('url').resolve
+    , request = require('request')
+    , assume = require('assume')
     , Primus = require('primus')
     , async = require('async')
+    , http = require('http')
     , omega = require('./');
 
   var port = 1024
+    , primus2
     , server2
-    , server
-    , http2
-    , http;
+    , primus
+    , server;
 
   beforeEach(function each(next) {
-    http = require('http').createServer();
-    http2 = require('http').createServer();
+    server = http.createServer();
+    server2 = http.createServer();
 
-    server = new Primus(http, {
+    primus = new Primus(server, {
       transformer: 'websockets'
     });
 
-    server2 = new Primus(http2, {
+    primus2 = new Primus(server2, {
       transformer: 'websockets'
     });
 
-    http.port = port++;
-    http2.port = port++;
+    server.port = port++;
+    server2.port = port++;
 
-    http.url = 'http://localhost:'+ http.port;
-    http2.url = 'http://localhost:'+ http2.port;
+    server.url = 'http://localhost:'+ server.port;
+    server2.url = 'http://localhost:'+ server2.port;
 
-    http.listen(http.port, function () {
-      http2.listen(http2.port, next);
+    server.listen(server.port, function () {
+      server2.listen(server2.port, next);
     });
   });
 
   afterEach(function each(next) {
-    server.destroy(function () {
-      server2.destroy(next);
+    primus.destroy(function () {
+      primus2.destroy(next);
     });
   });
 
@@ -48,41 +51,128 @@ describe('omega supreme', function () {
 
   it('exposes a plugin interface', function () {
     assume(omega.server).to.be.a('function');
-    server.use('omega-supreme', omega);
-  });
-
-  it('adds a middleware layer', function () {
-    assume(!~server.indexOfLayer('omega-supreme')).to.be.true();
-    server.use('omega-supreme', omega);
-    assume(!!~server.indexOfLayer('omega-supreme')).to.be.true();
+    primus.use('omega', omega);
   });
 
   describe('.options', function () {
-    it('returns a object with our defaults', function () {
+    it('returns an object with our defaults', function () {
       assume(omega.options()).to.be.a('object');
       assume(omega.options().method).to.be.a('string');
     });
 
     it('uppercases methods', function () {
-      var options = omega.options({ method: 'get' });
+      var options = omega.options({
+        url: '/primus/supreme/omega',
+        concurrently: 10,
+        password: 'foo',
+        username: 'bar',
+        method: 'get'
+      });
 
       assume(options.method).to.equal('GET');
       assume(omega.options().method).to.equal('PUT');
     });
   });
 
-  describe('.forward', function () {
-    it('adds a .forward method', function () {
-      assume(server.forward).to.be.undefined();
-      server.use('omega-supreme', omega);
-      assume(server.forward).to.be.a('function');
+  describe('middleware', function () {
+    it('adds a middleware layer', function () {
+      assume(!~primus.indexOfLayer('omega-supreme')).to.be.true();
+      primus.use('omega', omega);
+      assume(!!~primus.indexOfLayer('omega-supreme')).to.be.true();
+    });
+
+    it('does not handle invalid requests (wrong path)', function (next) {
+      primus.use('omega', omega);
+
+      request({
+        url: url(server.url, '/primus/wrong/path'),
+        auth: { user: 'omega', pass: 'supreme' },
+        method: 'PUT'
+      }, function (err, res, body) {
+        if (err) return next(err);
+
+        assume(res.statusCode).to.equal(426);
+        assume(body).to.equal(http.STATUS_CODES[426]);
+        next();
+      });
+    });
+
+    it('does not handle invalid requests (wrong method)', function (next) {
+      primus.use('omega', omega);
+
+      request({
+        url: url(server.url, '/primus/omega/supreme'),
+        auth: { user: 'omega', pass: 'supreme' }
+      }, function (err, res, body) {
+        if (err) return next(err);
+
+        assume(res.statusCode).to.equal(426);
+        assume(body).to.equal(http.STATUS_CODES[426]);
+        next();
+      });
+    });
+
+    it('does not handle invalid requests (missing headers)', function (next) {
+      primus.use('omega', omega);
+
+      request({
+        url: url(server.url, '/primus/omega/supreme'),
+        method: 'PUT'
+      }, function (err, res, body) {
+        if (err) return next(err);
+
+        assume(res.statusCode).to.equal(426);
+        assume(body).to.equal(http.STATUS_CODES[426]);
+        next();
+      });
+    });
+
+    it('handles unauthorized requests', function (next) {
+      primus.use('omega', omega);
+
+      request({
+        url: url(server.url, '/primus/omega/supreme'),
+        auth: { user: 'foo', pass: 'bar' },
+        method: 'PUT'
+      }, function (err, res, body) {
+        if (err) return next(err);
+
+        assume(res.statusCode).to.equal(401);
+        assume(body).to.contain('Feel the power of my lazers!');
+        next();
+      });
+    });
+
+    it('handles requests with an invalid body', function (next) {
+      primus.use('omega', omega);
+
+      request({
+        url: url(server.url, '/primus/omega/supreme'),
+        auth: { user: 'omega', pass: 'supreme' },
+        method: 'PUT',
+        body: 'foo'
+      }, function (err, res, body) {
+        if (err) return next(err);
+
+        assume(res.statusCode).to.equal(500);
+        assume(body).to.contain('invalid data structure');
+        next();
+      });
+    });
+  });
+
+  describe('forward', function () {
+    it('adds a forward method', function () {
+      assume(primus.forward).to.be.undefined();
+      primus.use('omega', omega);
+      assume(primus.forward).to.be.a('function');
     });
 
     it('returns the count of sparks matching (which is 0)', function (next) {
-      server.use('omega', omega);
-      server2.use('omega', omega);
+      primus.use('omega', omega);
+      primus2.use('omega', omega);
 
-      server.forward(http2.url, 'foo', 'unkown', function (err, data) {
+      primus.forward(server2.url, 'foo', 'unknown', function (err, data) {
         if (err) return next(err);
 
         assume(data.send).to.equal(0);
@@ -91,12 +181,12 @@ describe('omega supreme', function () {
     });
 
     it('returns the count of sparks matching (which is 1)', function (next) {
-      server.use('omega', omega);
-      server2.use('omega', omega);
+      primus.use('omega', omega);
+      primus2.use('omega', omega);
 
-      var client = server2.Socket(http2.url);
+      var client = primus2.Socket(server2.url);
       client.id(function get(id) {
-        server.forward(http2.url, 'foo', id, function (err, data) {
+        primus.forward(server2.url, 'foo', id, function (err, data) {
           if (err) return next(err);
 
           assume(data.send).to.equal(1);
@@ -105,12 +195,12 @@ describe('omega supreme', function () {
       });
     });
 
-    it('does not request server if sparks are all local', function (next) {
-      server2.use('omega', omega);
+    it('does not send a request when the sparks are all local', function (next) {
+      primus2.use('omega', omega);
 
-      var client = server2.Socket(http2.url);
+      var client = primus2.Socket(server2.url);
       client.id(function get(id) {
-        server2.forward(http2.url, 'foo', id, function (err, data) {
+        primus2.forward(server2.url, 'foo', id, function (err, data) {
           if (err) return next(err);
 
           assume(data.send).to.equal(1);
@@ -121,18 +211,18 @@ describe('omega supreme', function () {
       });
     });
 
-    it('merges local and server send counts', function (next) {
-      server.use('omega', omega);
-      server2.use('omega', omega);
+    it('broadcasts if no spark id is provided', function (next) {
+      primus.use('omega', omega);
+      primus2.use('omega', omega);
 
       var connections = [
-        server.Socket(http2.url), server2.Socket(http2.url),
-        server.Socket(http2.url), server2.Socket(http2.url),
-        server.Socket(http2.url), server2.Socket(http2.url),
-        server.Socket(http2.url), server2.Socket(http2.url),
-        server.Socket(http2.url), server2.Socket(http2.url),
-        server.Socket(http2.url), server2.Socket(http2.url),
-        server.Socket(http2.url), server2.Socket(http2.url)
+        primus2.Socket(server2.url), primus2.Socket(server2.url),
+        primus2.Socket(server2.url), primus2.Socket(server2.url),
+        primus2.Socket(server2.url), primus2.Socket(server2.url),
+        primus2.Socket(server2.url), primus2.Socket(server2.url),
+        primus2.Socket(server2.url), primus2.Socket(server2.url),
+        primus2.Socket(server2.url), primus2.Socket(server2.url),
+        primus2.Socket(server2.url), primus2.Socket(server2.url)
       ];
 
       async.each(connections, function (client, next) {
@@ -147,7 +237,44 @@ describe('omega supreme', function () {
           });
         }, next);
 
-        server.forward(http2.url, 'bar', function (err, data) {
+        primus.forward(server2.url, 'bar', function (err, data) {
+          if (err) return next(err);
+
+          assume(data.send).to.equal(connections.length);
+        });
+      });
+    });
+
+    it('merges local and server sent counts', function (next) {
+      primus.use('omega', omega);
+      primus2.use('omega', omega);
+
+      var connections = [
+        primus.Socket(server.url), primus2.Socket(server2.url),
+        primus.Socket(server.url), primus2.Socket(server2.url),
+        primus.Socket(server.url), primus2.Socket(server2.url),
+        primus.Socket(server.url), primus2.Socket(server2.url),
+        primus.Socket(server.url), primus2.Socket(server2.url),
+        primus.Socket(server.url), primus2.Socket(server2.url),
+        primus.Socket(server.url), primus2.Socket(server2.url)
+      ];
+
+      async.each(connections, function (client, next) {
+        client.on('open', next);
+      }, function (err) {
+        if (err) return next(err);
+
+        assume(primus.connected).to.equal(7);
+        assume(primus2.connected).to.equal(7);
+
+        async.each(connections, function (client, next) {
+          client.on('data', function (msg) {
+            assume(msg).to.equal('bar');
+            next();
+          });
+        }, next);
+
+        primus.forward(server2.url, 'bar', function (err, data) {
           if (err) return next(err);
 
           assume(data.send).to.equal(connections.length);
@@ -156,13 +283,13 @@ describe('omega supreme', function () {
     });
 
     it('sends the data to one connected client', function (next) {
-      server.use('omega', omega);
-      server2.use('omega', omega);
+      primus.use('omega', omega);
+      primus2.use('omega', omega);
 
-      var client = server2.Socket(http2.url);
+      var client = primus2.Socket(server2.url);
 
       client.id(function get(id) {
-        server.forward(http2.url, 'foo', id, function (err, data) {
+        primus.forward(server2.url, 'foo', id, function (err) {
           if (err) return next(err);
         });
       });
@@ -173,54 +300,82 @@ describe('omega supreme', function () {
       });
     });
 
-    it('does not error when given an empty url', function (next) {
-      server.use('omega', omega);
-      server2.use('omega', omega);
-
-      var client = server2.Socket(http2.url);
-
-      client.id(function get(id) {
-        server.forward(null, 'foo', id, function (err, data) {
-          assume(err.message).to.equal('No servers provided');
-          assume(data.send).to.equal(0);
-          assume(data.ok).to.be.false();
-          next();
-        });
-      });
-
-    });
-
-    it('broadcasts if no spark id is provided', function (next) {
-      server.use('omega', omega);
-      server2.use('omega', omega);
+    it('sends the data to some connected clients', function (next) {
+      primus.use('omega', omega);
+      primus2.use('omega', omega);
 
       var connections = [
-        server2.Socket(http2.url), server2.Socket(http2.url),
-        server2.Socket(http2.url), server2.Socket(http2.url),
-        server2.Socket(http2.url), server2.Socket(http2.url),
-        server2.Socket(http2.url), server2.Socket(http2.url),
-        server2.Socket(http2.url), server2.Socket(http2.url),
-        server2.Socket(http2.url), server2.Socket(http2.url),
-        server2.Socket(http2.url), server2.Socket(http2.url)
-      ], donish = 1;
+        primus.Socket(server.url), primus2.Socket(server2.url),
+        primus.Socket(server.url), primus2.Socket(server2.url)
+      ], received = 0;
+
+      connections.forEach(function (client, index) {
+        client.on('data', function (msg) {
+          if (index < 2) throw new Error('This client should be excluded');
+
+          assume(msg).to.equal('foo');
+          if (++received === 2) next();
+        });
+      });
 
       async.each(connections, function (client, next) {
         client.on('open', next);
       }, function (err) {
         if (err) return next(err);
 
-        async.each(connections, function (client, next) {
-          client.on('data', function (msg) {
-            assume(msg).to.equal('bar');
-            next();
+        async.map(connections, function (client, next) {
+          client.id(function (id) {
+            next(undefined, id);
           });
-        }, next);
+        }, function (err, ids) {
+          if (err) return next (err);
 
-        server.forward(http2.url, 'bar', function (err, data) {
-          if (err) return next(err);
-
-          assume(data.send).to.equal(connections.length);
+          //
+          // Fake id to simulate an additional client on a third server.
+          //
+          ids.push('id');
+          primus.forward([server2.url], 'foo', ids.slice(2), function (err) {
+            if (err) return next(err);
+          });
         });
+      });
+    });
+
+    it('bails out when forwarding to an empty url', function (next) {
+      primus.use('omega', omega);
+      primus2.use('omega', omega);
+
+      var client = primus2.Socket(server2.url);
+
+      client.id(function get(id) {
+        primus.forward(null, 'foo', id, function (err, data) {
+          assume(err.message).to.equal('No servers provided');
+          assume(data.send).to.equal(0);
+          assume(data.ok).to.be.false();
+          next();
+        });
+      });
+    });
+
+    it('receives an error when forwarding to a dead server', function (next) {
+      primus.use('omega', omega);
+
+      primus.forward('http://localhost:1024', 'foo', function (err) {
+        assume(err).to.be.instanceOf(Error);
+        assume(err.packet).to.be.a('object');
+        assume(err.packet.msg).to.equal('foo');
+        next();
+      });
+    });
+
+    it('receives an error when forwarding an undefined message', function (next) {
+      primus.use('omega', omega);
+      primus2.use('omega', omega);
+
+      primus.forward(server2.url, undefined, function (err) {
+        assume(err).to.be.instanceOf(Error);
+        assume(err.message).to.equal('invalid data structure');
+        next();
       });
     });
   });
